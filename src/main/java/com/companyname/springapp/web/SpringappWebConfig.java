9dfde8a5dec2e3b6cb59a9639502b614c8662e66
Ignacio Sanchez <ignacio.sanchez@comafi.com.ar>
2021-10-11 11:39:52 -0300

    separo la vista del controlador

--- src/main/java/com/companyname/springapp/web/SpringappWebConfig.java
+++ src/main/java/com/companyname/springapp/web/SpringappWebConfig.java
@@ -3,10 +3,27 @@ package com.companyname.springapp.web;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
 public class SpringappWebConfig {
 
+	/**
+	 * Puedes crear un mapeo basico entre una vista y una localizacion simplemente
+	 * configurando un prefijo y un sufijo en un bean InternalResourceViewResolver
+	 * definido en el contexto del ServletDispatcher.
+	 * 
+	 * @return
+	 */
+	@Bean
+	public ViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
+	}
 }
