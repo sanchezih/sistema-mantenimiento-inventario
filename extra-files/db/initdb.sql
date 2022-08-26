7b4e9bd26cd0e18bfc195ce0eb6e6028fa08888e
Ignacio Sanchez <sanchezih@gmail.com>
2022-08-26 20:55:09 -0300

    update

--- extra-files/db/initdb.sql
+++ extra-files/db/initdb.sql
@@ -1,15 +1,14 @@
 DROP DATABASE IF EXISTS springapp;
-CREATE DATABASE springapp;
-USE springapp;
+CREATE DATABASE springapp; USE springapp;
 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
-  id INTEGER PRIMARY KEY,
-  description varchar(255),
-  price decimal(15,2)
+ id INTEGER PRIMARY KEY,
+ description VARCHAR(255),
+ price DECIMAL(15,2)
 );
 CREATE INDEX products_description ON products(description);
 
-INSERT INTO products (id, description, price) values(1, 'Lamp', 5.78);
-INSERT INTO products (id, description, price) values(2, 'Table', 75.29);
-INSERT INTO products (id, description, price) values(3, 'Chair', 22.81);
+INSERT INTO products (id, description, price) VALUES(1, 'Lamp', 5.78);
+INSERT INTO products (id, description, price) VALUES(2, 'Table', 75.29);
+INSERT INTO products (id, description, price) VALUES(3, 'Chair', 22.81);
